540a3b01dd68fa6e87a538edb0539648b40c2818
Ana Rocha <anasoledad.rocha@gmail.com>
2023-05-22 00:07:44 -0300

    Se agregó el título y el footer al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css'
+import { Container } from 'react-bootstrap';
 
 function App() {
 
 
   return (
     <>
-      <div>
-      
-      </div>
+      <Container className='pt-5 mainPage'>
+          <h3>Administrar colores</h3>
+
+      </Container>
+      <footer className='bg-dark text-light text-center py-4'>
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
     
     </>
   )
