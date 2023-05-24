592e54b598990d53a8aecdef289f249ff5fdf4e2
Ana Rocha <anasoledad.rocha@gmail.com>
2023-05-24 02:45:58 -0300

    Se puede guardar colores en un array

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,15 +1,27 @@
 import {Form, Button} from 'react-bootstrap'
 import ContenedorCard from './ContenedorCard';
+import { useState } from 'react';
 
 const Formulario = () => {
+
+  const [color, setColor] = useState('');
+  const [colores, setColores] = useState([]);
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    setColores([...colores, color]);
+    setColor('');
+  }
+
   return (
     <section className='mx-3 mx-md-5'>
       
-      <Form>
-        <Form.Group className="mb-3" controlId="tarea">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="color">
           <div className='d-flex'>
             <div className='cuadroColorForm border border-dark me-2 me-md-4'></div>
-            <Form.Control type="texto" placeholder="Ingrese un color ej Blue" />
+            <Form.Control type="texto" placeholder="Ingrese un color ej Blue" onChange={(e)=> setColor(e.target.value)}
+          value={color}/>
           </div>
 
           <div className='d-flex justify-content-end'>
